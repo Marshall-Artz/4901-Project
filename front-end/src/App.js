56ae620d7b9bc305e4776546332348075288c5eb
Brandon Lopez <brandon.lopez1214@gmail.com>
2022-11-02 02:20:04 -0500

    Changed 'Text' to 'InputBox' & added 'OutputBox'

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Banner from './components/Banner';
-import Text from './components/Text';
+import InputBox from './components/InputBox';
+import OutputBox from './components/OutputBox';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Banner/>
-        <Text/>
+        <Banner/> 
+        <InputBox/> 
+        <OutputBox/>
       </header>
     </div>
   );
-}
+} 
 
 export default App;
--- front-end/src/components/OutputBox.jsx
+++ front-end/src/components/OutputBox.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
-import { Box } from '@mui/system';
+// import { Box } from '@mui/system'; //commented out because although declared, box is not used
 
 import '../common/GlobalStyles.css';
 
-const Text = () => {
+// Changed 'Text' to 'OutputBox'
+const OutputBox = () => {
   return (
     <TextareaAutosize className='Input'
       aria-label="empty textarea"
-      placeholder="Enter text here!"
+      placeholder="This is where code appears! (Output field)"
       minRows={20}
     />
   );
 }
 
-export default Text;
+export default OutputBox;
