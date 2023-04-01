4d32eb4701cdecd76d99348cb6ef91235156ab4c
Brandon Lopez <brandon.lopez1214@gmail.com>
2023-03-31 21:43:06 -0500

    Smart tips beta added

--- ttc/src/components/ProductDescription.js
+++ ttc/src/components/ProductDescription.js
@@ -5,7 +5,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 const { Configuration, OpenAIApi } = require("openai");
 const LRU = require("lru-cache");
-//FIXED
+
 class ProductDescription extends Component {
   constructor() {
     super();
@@ -13,8 +13,10 @@ class ProductDescription extends Component {
       model: "text-davinci-003",
       heading: "AI Output shown below",
       response: "... awaiting input ...",
-      aiActivated: false, // track whether AI has been activated
-      showRerunButton: false // track whether to show the 'rerun' button
+      aiActivated: false,
+      showRerunButton: false,
+      codeDescription: "",
+      codeExplanation: "",
     };
     this.cache = new LRU({ max: 100 });
   }
@@ -22,13 +24,8 @@ class ProductDescription extends Component {
   onFormSubmit = (e) => {
     e.preventDefault();
 
-    if (!(e.target instanceof HTMLFormElement)) {
-      console.error("Invalid form element");
-      return;
-    }
-
-    const formData = new FormData(e.target),
-      formDataObj = Object.fromEntries(formData.entries());
+    const formData = new FormData(e.target);
+    const formDataObj = Object.fromEntries(formData.entries());
     console.log(formDataObj.productName);
 
     const configuration = new Configuration({
@@ -47,81 +44,127 @@ class ProductDescription extends Component {
         presence_penalty: 0,
       })
       .then((response) => {
-        this.setState({
-          heading: `AI TTC output below`,
-          response: `${response.data.choices[0].text}`,
-          aiActivated: true,
-          showRerunButton: true // set showRerunButton to true after AI has been activated
-        });
+        this.setState(
+          {
+            heading: `AI TTC output below`,
+            response: `${response.data.choices[0].text}`,
+            aiActivated: true,
+            showRerunButton: true,
+          },
+          () => {
+            this.setState({ codeDescription: formDataObj.productName });
+          }
+        );
       });
   };
 
-  switchModel = (newModel, event = null) => {
-    this.setState({ model: newModel, showRerunButton: true }, () => {
-      // re-run the form submission with the new model
-      if (event && event.target instanceof HTMLFormElement) {
-        event.preventDefault();
-        this.onFormSubmit(event);
-      } else {
-        const form = document.querySelector("form");
-        if (form) {
-          const newEvent = new Event("submit", { bubbles: true });
-          Object.defineProperty(newEvent, "target", {
-            value: form,
-            enumerable: true,
-          });
-          form.dispatchEvent(newEvent);
-        } else {
-          console.error("Form element not found");
-        }
-      }
+  onExplainSubmit = (e) => {
+    e.preventDefault();
+    this.explainCode(this.state.response);
+  };
+
+  explainCode = (codeSnippet) => {
+    const { codeDescription } = this.state;
+    if (!codeDescription) {
+      return;
+    }
+    const configuration = new Configuration({
+      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
+    const openai = new OpenAIApi(configuration);
+  
+    openai
+      .createCompletion({
+        model: "text-davinci-002",
+        prompt: `I have a code snippet and a description. Please explain the syntax of the code and why it works in a way that someone who doesn't understand how to read code can comprehend.\n\nCode Snippet:\n${codeSnippet}\n\nDescription: ${codeDescription}\n\nSyntax Explanation:`,
+        temperature: 0.5,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      })
+      .then((response) => {
+        const explainCodeResponse = response.data.choices[0].text;
+        console.log("Explain Code Response:", explainCodeResponse);
+        this.setState({ codeExplanation: explainCodeResponse });
+      });
   };
   
+  
   render() {
-  const { model, heading, response, aiActivated, showRerunButton } = this.state;
-
-  return (
-    <div>
-      <Container>
-        <br />
-        <br />
-        <h1 style={{ fontFamily: 'Verdana', fontSize: '32px', textTransform: 'uppercase' }}>Welcome to text-TO-code</h1>
-        <br />
-        <br />
-        <br />
-        <Form onSubmit={this.onFormSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label style={{ fontFamily: 'Verdana', fontSize: '18px' }}>Insert TEXT you would like to convert to CODE</Form.Label>
-            <Form.Control as="textarea" rows={4} name="productName" placeholder="Enter Text" style={{ backgroundColor: '#000000', color: '#ffffff', verticalAlign: 'top', outline: 'none', resize: 'none' }} />
-            <Form.Text className="text-muted">You can be as simple / detailed as you like!</Form.Text>
-          </Form.Group>
-          <Button variant="primary" size="lg" type="submit">
-            Launch AI
-          </Button>
-        </Form>
-
-        <br />
-        <br />
-        <Card style={{ backgroundColor: '#000000', color: '#ffffff' }}>
-          <Card.Body>
-            <Card.Title>
-              <h1>{heading}</h1>
-            </Card.Title>
-            <hr />
-            <br />
-            <Card.Text style={{ fontFamily: 'Consolas, Monaco, \'Andale Mono\', \'Ubuntu Mono\', monospace', fontSize: '16px', whiteSpace: 'pre-wrap', wordWrap: 'break-word', lineHeight: '1.5' }}>
-              <SyntaxHighlighter language="javascript" style={tomorrow}>
-                {response}
-              </SyntaxHighlighter>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Container>
-
-      <br /><br /><br /><br />
-    </div>
-  );
-}
+    const { model, heading, response, aiActivated, showRerunButton, codeDescription, codeExplanation } = this.state;
+  
+    return (
+      <div>
+        <Container>
+          <br />
+          <br />
+          <h1 style={{ fontFamily: "Verdana", fontSize: "32px", textTransform: "uppercase" }}>Welcome to text-TO-code</h1>
+          <br />
+          <br />
+          <br />
+          <Form onSubmit={this.onFormSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label style={{ fontFamily: "Verdana", fontSize: "18px" }}>Insert TEXT you would like to convert to CODE</Form.Label>
+              <Form.Control as="textarea" rows={4} name="productName" placeholder="Enter Text" style={{ backgroundColor: "#000000", color: "#ffffff", verticalAlign: "top", outline: "none", resize: "none" }} onChange={this.handleDescriptionChange} />
+              <Form.Text className="text-muted">You can be as simple / detailed as you like!</Form.Text>
+            </Form.Group>
+            <Button variant="primary" size="lg" type="submit">
+              Launch AI
+            </Button>
+          </Form>
+  
+          <br />
+          <br />
+          <Card style={{ backgroundColor: "#000000", color: "#ffffff" }}>
+            <Card.Body>
+              <Card.Title>
+                <h1>{heading}</h1>
+              </Card.Title>
+              <hr />
+              <br />
+              <Card.Text style={{ fontFamily: "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace", fontSize: "16px", whiteSpace: "pre-wrap", wordWrap: "break-word", lineHeight: "1.5" }}>
+                <SyntaxHighlighter language="javascript" style={tomorrow}>
+                  {response}
+                </SyntaxHighlighter>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+  
+          {aiActivated && (
+            <div>
+              <br />
+              <br />
+              <Form onSubmit={this.onExplainSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label style={{ fontFamily: "Verdana", fontSize: "18px" }}>Explain the code</Form.Label>
+                  <Button variant="primary" size="lg" type="submit">
+                    Explain
+                  </Button>
+                </Form.Group>
+              </Form>
+  
+              <br />
+              <br />
+              <Card style={{ backgroundColor: "#000000", color: "#ffffff" }}>
+                <Card.Body>
+                  <Card.Title>
+                    <h1>Explanation</h1>
+                  </Card.Title>
+                  <hr />
+                  <br />
+                  <Card.Text style={{ fontFamily: "Verdana, Geneva, Tahoma, sans-serif", fontSize: "16px" }}>
+                    {codeExplanation}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          )}
+        </Container>
+  
+        <br /><br /><br /><br />
+      </div>
+    );
+  }
 } 
 export default ProductDescription;
