54c7b91afdf0d097d3d6238390b528fd23b8bc58
short_name <email>
2023-04-02 00:11:15 -0500

    modified navigation elements to highlight the select page

--- ttc/src/components/navigation.js
+++ ttc/src/components/navigation.js
@@ -1,35 +1,65 @@
-import React from "react"
-import { Component } from "react"
-import 'bootstrap/dist/css/bootstrap.css'
-import { Nav, Navbar } from 'react-bootstrap'
-import logo from '../logo.png';
+import React from "react";
+import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom"; // import the Link component from React Router
+import logo from "../logo.png";
 
 
-// ADD LOGO ABOVE 'text TO code'
-// <img src = {logo} width="50px" />
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeNavOption: "home", // set the default active option
+    };
+    this.handleNavOptionClick = this.handleNavOptionClick.bind(this);
+  }
 
-class Navigation extends Component{
-    render() {
-        return(
-            <div>
-                <Navbar bg = "dark" variant="dark" sticky="top" expand="md" collapseOnSelect>
-                <Navbar.Brand href="/">
-                <img alt={"logo"} src = {logo} width="50px" />
-                   &nbsp;&nbsp;&nbsp; Home
-                </Navbar.Brand>
+  handleNavOptionClick(option) {
+    this.setState({ activeNavOption: option }); 
+  }
 
-                    <Navbar.Toggle />
+  render() {
+    return (
+      <div>
+        <Navbar
+          bg="dark"
+          variant="dark"
+          sticky="top"
+          expand="md"
+          collapseOnSelect
+        >
+          <Navbar.Brand href="/">
+            <img alt={"logo"} src={logo} width="50px" />
+            
+          </Navbar.Brand>
 
-                    <Navbar.Collapse>
-                        <Nav>
-                            <Nav.Link href="products-description"> Code Display</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
+          <Navbar.Toggle />
 
-                </Navbar>
-            </div>
-        )
-    }
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link
+                as={Link} 
+                to="/" 
+                active={this.state.activeNavOption === "home"} 
+                onClick={() => this.handleNavOptionClick("home")} 
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                as={Link} 
+                to="/products-description" 
+                active={this.state.activeNavOption === "code"} 
+                onClick={() => this.handleNavOptionClick("code")} 
+              >
+                Code Display
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default Navigation
+export default Navigation;
