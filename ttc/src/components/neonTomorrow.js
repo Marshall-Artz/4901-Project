4d5ea8f7386f374418b9386de0c31aefd9df1b20
Brandon Lopez <brandon.lopez1214@gmail.com>
2023-04-04 01:30:06 -0500

    key

--- ttc/src/components/neonTomorrow.js
+++
@@ -1,9 +0,0 @@
-import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
-
-export const neonTomorrow = {
-  ...tomorrow,
-  'code[class*="language-"]': {
-    ...tomorrow['code[class*="language-"]'],
-    color: '#FFFFFF' // aqua
-  }
-};
