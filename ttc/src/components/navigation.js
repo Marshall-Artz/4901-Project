793d77f8f795820aef50c83d095e6412bd0c03ea
Brandon Lopez <brandon.lopez1214@gmail.com>
2023-04-04 13:31:35 -0500

    halejuiah

---
+++ ttc/src/components/navigation.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Component } from "react"
+import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar } from 'react-bootstrap'
+import logo from "../logo.svg"
+
+
+// ADD LOGO ABOVE 'text TO code'
+// <img src = {logo} width="50px" />
+
+class Navigation extends Component{
+    render() {
+        return(
+            <div>
+                <Navbar bg = "dark" variant="dark" sticky="top" expand="md" collapseOnSelect>
+                <Navbar.Brand href="/">
+                <img alt={"logo"} style={{ backgroundColor: "aqua", marginLeft: '20px'}}  src = {logo} width="50px" />
+                   &nbsp;&nbsp;&nbsp; Home
+                </Navbar.Brand>
+
+                    <Navbar.Toggle />
+
+                    <Navbar.Collapse>
+                        <Nav>
+                            <Nav.Link href="products-description"> Code Display</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default Navigation
