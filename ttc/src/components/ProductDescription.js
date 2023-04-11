3212450e9acb1eb59569e2a01de5324883dace18
sana5701 <sana5701@gmail.com>
2023-04-10 19:34:10 -0500

    updated output and input boxes

--- ttc/src/components/ProductDescription.js
+++ ttc/src/components/ProductDescription.js
@@ -100,6 +100,7 @@ class ProductDescription extends Component {
                       </div>
                     </div>
 
+
                   </Resizable>
                 </Draggable>
               </Form.Group>
